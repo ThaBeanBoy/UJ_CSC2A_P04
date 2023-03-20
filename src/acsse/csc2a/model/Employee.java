10f0b860b84afc9b05026073c6e9fda5bb3f62bb
ThaBeanBoy <gtchipoyera@gmail.com>
2023-03-20 20:02:20 +0200

    The EncryptedMessage inherits from the Message class & also properly implements the validate function from the IValidation interface

--- src/acsse/csc2a/model/Employee.java
+++ src/acsse/csc2a/model/Employee.java
@@ -1,6 +1,7 @@
 package acsse.csc2a.model;
 
 import acsse.csc2a.model.IValidation;
+import acsse.csc2a.model.Message;
 
 public class Employee implements IValidation{
 
--- src/acsse/csc2a/model/EncryptedMessage.java
+++ src/acsse/csc2a/model/EncryptedMessage.java
@@ -1,12 +1,33 @@
 package acsse.csc2a.model;
 
 import acsse.csc2a.model.IValidation;
+import acsse.csc2a.model.Message;
+import acsse.csc2a.model.RECIPIENT_TYPE;
 
-public class EncryptedMessage  implements  IValidation{
+public class EncryptedMessage extends Message implements  IValidation{
+    public String Key;
+    public EncryptedMessage(
+            String iD,
+            String contents,
+            PLANET_TYPE planet_source,
+            PLANET_TYPE planet_destination,
+            MESSAGE_TYPE message_type,
+            String Key
+    )
+    {
+        super(iD, contents, planet_source, planet_destination, message_type);
+        this.Key = Key;
+    }
 
     //Implementing Validation Interface
     @Override
     public boolean validate() {
-        return false;
+        return (Key.length() > 10);
+    }
+
+    public static class KeyLengthErr extends Exception{
+        public KeyLengthErr() {
+            super("errorMessage");
+        }
     }
 }
