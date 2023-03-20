ef747e013672f09671aa34efc72f55a6195a539d
ThaBeanBoy <gtchipoyera@gmail.com>
2023-03-20 20:17:27 +0200

    Made SOSMessage inherit from Message class, & implemented the Validation method

--- src/acsse/csc2a/model/SOSMessage.java
+++ src/acsse/csc2a/model/SOSMessage.java
@@ -1,12 +1,30 @@
 package acsse.csc2a.model;
 
 import acsse.csc2a.model.IValidation;
+import acsse.csc2a.model.Message;
+import acsse.csc2a.model.RECIPIENT_TYPE;
 
-public class SOSMessage implements IValidation {
+public class SOSMessage extends Message implements IValidation {
+    private final RECIPIENT_TYPE Recipient;
+
+    public SOSMessage(
+            String iD,
+            String contents,
+            PLANET_TYPE planet_source,
+            PLANET_TYPE planet_destination,
+            MESSAGE_TYPE message_type,
+            RECIPIENT_TYPE Recipient
+    ){
+        super(iD, contents, planet_source, planet_destination, message_type);
+        this.Recipient = Recipient;
+    }
 
     //Implementing Validation Interface
     @Override
     public boolean validate() {
-        return false;
+        return switch(this.Recipient){
+            case GOVERNMENT, PUBLIC ->  true;
+            default -> false;
+        };
     }
 }
