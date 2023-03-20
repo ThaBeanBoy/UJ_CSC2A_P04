f4ffc0e1ead7f6ee72b3d6f8c9664a69bebccf29
ThaBeanBoy <gtchipoyera@gmail.com>
2023-03-20 21:56:44 +0200

    Finished implementing the Employee class

--- src/acsse/csc2a/model/Employee.java
+++ src/acsse/csc2a/model/Employee.java
@@ -1,14 +1,22 @@
 package acsse.csc2a.model;
 
 import acsse.csc2a.model.IValidation;
-import acsse.csc2a.model.Message;
+import acsse.csc2a.model.Ship;
 
 public class Employee implements IValidation{
+    private String EmployeeID;
+    private String FirstName;
+    private String LastName;
+    private Ship ShipData;
 
+    public String getEmployeeID(){return this.EmployeeID; }
+    public String getFirstName(){return this.FirstName;}
+    public String getLastName(){return this.LastName;}
+    public Ship getShipData(){return this.ShipData;}
 
     //Implementing Validation Interface
     @Override
     public boolean validate() {
-        return false;
+        return this.EmployeeID.length() >= 6;
     }
 }
