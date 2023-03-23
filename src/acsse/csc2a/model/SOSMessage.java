26d90a850f83e225f8eed2dbe9a19fa3ed6cdd61
ThaBeanBoy <gtchipoyera@gmail.com>
2023-03-23 22:59:54 +0200

    Added javadocs to SOSMessage

--- src/acsse/csc2a/model/SOSMessage.java
+++ src/acsse/csc2a/model/SOSMessage.java
@@ -4,9 +4,24 @@ import acsse.csc2a.model.IValidation;
 import acsse.csc2a.model.Message;
 import acsse.csc2a.model.RECIPIENT_TYPE;
 
+/**
+ * A form of message
+ * @author TG Chipoyera
+ * @version P04
+ * @see Message,IValidation,RECIPIENT_TYPE
+ */
 public class SOSMessage extends Message implements IValidation {
     private final RECIPIENT_TYPE Recipient;
 
+    /**
+     * Constructs an instance of SOSMessage
+     * @param iD ID of the Message
+     * @param contents The contents of the message
+     * @param planet_source The planet where the message is coming from
+     * @param planet_destination The planet where the message is destined for
+     * @param message_type The type of message
+     * @param Recipient The recipient
+     */
     public SOSMessage(
             String iD,
             String contents,
@@ -19,9 +34,18 @@ public class SOSMessage extends Message implements IValidation {
         this.Recipient = Recipient;
     }
 
+    /**
+     * Returns the recipient
+     * @return RECIPIENT_TYPE
+     */
     public final RECIPIENT_TYPE getRecipient(){ return this.Recipient; }
 
     //Implementing Validation Interface
+
+    /**
+     * Validates the SOSMessage
+     * @return boolean
+     */
     @Override
     public boolean validate() {
         return switch(this.Recipient){
