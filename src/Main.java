67c3de47f71709cc2d4ef519d4d71b66accca9f2
ThaBeanBoy <gtchipoyera@gmail.com>
2023-03-23 15:19:40 +0200

    readShip method can properly read the ship.dat file & properly generate the Ship name, even if there is a space in the ship name :)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import acsse.csc2a.model.*;
 import acsse.csc2a.file.DataReader;
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello world");
-    DataReader.readShip("data/ship.dat", "data/messages.dat");
+        Ship ShipInDat = DataReader.readShip("data/ship.dat", "data/messages.dat");
+        System.out.println(ShipInDat.getName());
     }
 }
--- src/acsse/csc2a/file/DataReader.java
+++ src/acsse/csc2a/file/DataReader.java
@@ -1,22 +1,40 @@
 package acsse.csc2a.file;
 
 // Binary IO classes
-import java.io.*;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.DataInputStream;
+// Binary IO exceptions
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+// acsse classes
 import acsse.csc2a.model.Ship;
+import acsse.csc2a.model.Message;
 
 public class DataReader {
     public static Ship readShip(String shipFilePath, String MessagesFilePath){
+        String ShipID = "";
+        StringBuilder ShipName = new StringBuilder();
+
         try(
             //Creating streams
             InputStream ShipStream = new FileInputStream(shipFilePath);
             InputStream MessagesStream = new FileInputStream(MessagesFilePath);
         ){
-            BufferedInputStream ShipBufferStream = new BufferedInputStream(ShipStream);
-            BufferedInputStream MessagesInputStream = new BufferedInputStream(MessagesStream);
+            DataInputStream ShipBufferStream = new DataInputStream(ShipStream);
+            DataInputStream MessagesInputStream = new DataInputStream(MessagesStream);
 
-            System.out.println(ShipBufferStream.available());
-        } catch (FileNotFoundException e) {
+            // Reading Ship Details
+            ShipID = ShipBufferStream.readUTF();
+            while(true){ //The file itself has an end, so it's fine to make a while loop that has true inside it
+                ShipName.append(String.format(" %s", ShipBufferStream.readUTF()));
+            }
+        }catch (EOFException e){
+            return new Ship(ShipID, ShipName.toString());
+        }
+        catch (FileNotFoundException e) {
             System.err.printf("""
                     Ship File Path: %s
                     Messages File Path: %s
@@ -27,7 +45,5 @@ public class DataReader {
             System.err.println("IOException exception!");
             throw new RuntimeException(e);
         }
-
-        return new Ship("SH1111", "Hello World");
     }
 }
