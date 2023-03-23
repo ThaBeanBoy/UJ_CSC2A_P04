30c195d095651b3028b03614ce2b4ecb4e15585f
ThaBeanBoy <gtchipoyera@gmail.com>
2023-03-23 19:04:00 +0200

    Made the employee constructor

--- src/acsse/csc2a/model/Employee.java
+++ src/acsse/csc2a/model/Employee.java
@@ -9,6 +9,17 @@ public class Employee implements IValidation{
     private String LastName;
     private Ship ShipData;
 
+    public Employee(String EmployeeID, String FirstName, String LastName, Ship Ship) throws Exception{
+        if(EmployeeID.length() >= 6)
+            throw new Exception();
+
+        this.EmployeeID = EmployeeID;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+    }
+
+
+
     public String getEmployeeID(){return this.EmployeeID; }
     public String getFirstName(){return this.FirstName;}
     public String getLastName(){return this.LastName;}
